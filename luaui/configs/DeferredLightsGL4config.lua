WatchTheFort/BAR-minified

commit 8a0e7fea1f3f14ecad925f9bf0c54aff536adc87

Core Stealth Tank (#3541)
* Initial commit of Core Stealth Tank

* Updated siegebreaker dps and firing animations

* Removed Siegebreaker recoil, tuned leading to compensate for new mechanics

* More siegebreaker retunings for new mechanic

luaui/configs/DeferredLightsGL4config.lua
@@ -9783,6 +9783,18 @@ local unitLights = {
 							lifetime = 0, sustain = 0, animtype = 0},
 		},
 	},
+	'corphantom'] = {
+		eyes1 = {
+			lightType = 'point',
+			pieceName = 'body',
+			lightConfig = { posx = 0, posy = 19, posz = 4, radius = 20,
+							color2r = 1, color2g = 0.2, color2b = 0.2, colortime = 0,
+							r = 1, g = 0.2, b = 0.2, a = 0.12,
+							modelfactor = 0.4, specular = 0, scattering = 1, lensflare = 0,
+							lifetime = 0, sustain = 0, animtype = 0},
+		},
+	},
+	
 	['corparrow'] = {
 		eyes1 = {
 			lightType = 'point',

gamedata/alldefs_post.lua
@@ -366,6 +366,8 @@ function UnitDef_Post(name, uDef)
 		if name == "coravp" then
 			local numBuildoptions = #uDef.buildoptions
 			uDef.buildoptions[numBuildoptions + 1] = "corvac" --corprinter
+			uDef.buildoptions[numBuildoptions + 2] = "corphantom"
+			uDef.buildoptions[numBuildoptions + 3] = "corsiegebreaker"
 		end
 		if name == "legavp" then
 			local numBuildoptions = #uDef.buildoptions
@@ -433,8 +435,9 @@ function UnitDef_Post(name, uDef)
 			uDef.buildoptions[numBuildoptions+3] = "corftiger"
 			uDef.buildoptions[numBuildoptions+4] = "cortorch"
 			uDef.buildoptions[numBuildoptions+5] = "corsiegebreaker"
+			uDef.buildoptions[numBuildoptions+6] = "corphantom"
 			if (printerpresent==false) then -- assuming sala and vac stay paired, this is tidiest solution
-				uDef.buildoptions[numBuildoptions+6] = "corvac" --corprinter
+				uDef.buildoptions[numBuildoptions+7] = "corvac" --corprinter
 
 			end
 		elseif name == "coraap" then

gamedata/armordefs.lua
@@ -238,6 +238,7 @@ local armorDefs = {
 		"cormwall",
 		"cornecro",
 		"corplat",
+		"corphantom",
 		"corprinter",
 		"corvac",
 		"corvacct",

gamedata/icontypes.lua
@@ -1679,6 +1679,10 @@ local icontypes = {
 		bitmap = "icons/corparrow.png",
 		size = 1.62749982
 	},
+	corphantom = {
+		bitmap = "icons/spy.png",
+		size = 1.20749986
+	},	
 	corplat = {
 		bitmap = "icons/factory_air_sea.png",
 		size = 1.52250004

gamedata/movedefs.lua
@@ -76,7 +76,7 @@ local moveDatas = {
 	-- 	maxwaterslope = 80,
 	-- },
 	-- amphibious tanks
-	-- corgarp armbeaver armmar corparrow armprow corseal corsala cormuskrat armcroc armpincer corintr legassistdrone_land corassistdrone armassistdrone legotter
+	-- corgarp armbeaver armmar corparrow armprow corseal corsala cormuskrat armcroc armpincer corintr legassistdrone_land corassistdrone armassistdrone legotter corphantom
 	ATANK3 = {
 		crushstrength = 30,
 		depthmod = 0,

luarules/configs/gui_soundeffects.lua
@@ -2116,6 +2116,11 @@ GUIUnitSoundEffects = {
 		BaseSoundMovementType = "cor-tnk-large-ok",
 		BaseSoundWeaponType = "con-assist",
 	},
+	corphantom = {
+		BaseSoundSelectType = "cor-tnk-medium-amph-sel",
+		BaseSoundMovementType = "cor-tnk-medium-amph-ok",
+		BaseSoundWeaponType = "cloak",
+	},	
 	-- FUN MODE - made possible by Teifion and Basic
 	-- corfav = {
 	-- BaseSoundSelectType   = "cor-veh-tiny-sel",

luarules/gadgets/unit_continuous_aim.lua
@@ -55,6 +55,7 @@ local convertedUnitsNames = {
 	['armvang'] = 3,
 	['armcrus'] = 5,
 	['corsala'] = 6,
+	['corsiegebreaker'] = 5,
 
 	-- the following units get a faster reaimtime to counteract their turret acceleration
 	['armthor'] = 4,

luaui/Widgets/gui_options.lua
@@ -5977,9 +5977,11 @@ function init()
 		[UnitDefNames["corsktl"].id] = false,
 		[UnitDefNames["armgremlin"].id] = true,
 		[UnitDefNames["armamex"].id] = true,
+		[UnitDefNames["armshockwave"].id] = true,
 		[UnitDefNames["armckfus"].id] = true,
 		[UnitDefNames["armspy"].id] = true,
 		[UnitDefNames["corspy"].id] = true,
+		[UnitDefNames["corphantom"].id] = true,
 	}
 	local unitdefConfig = {}
 	if WG['autocloak'] ~= nil then

commit 43e08303d3ea06a59012df60307696622b1751d6

Unslave unitfinished lights from the base piece (#3543)
* Unslave unitfinished lights from the base piece

* Fix deferred lights broken by #3541

luaui/configs/DeferredLightsGL4config.lua
@@ -9783,7 +9783,7 @@ local unitLights = {
 							lifetime = 0, sustain = 0, animtype = 0},
 		},
 	},
-	'corphantom'] = {
+	['corphantom'] = {
 		eyes1 = {
 			lightType = 'point',
 			pieceName = 'body',
@@ -21291,7 +21291,7 @@ local unitEventLightsNames = {
 		default = {
 			default = {
 				lightType = 'cone',
-				pieceName = 'base',
+				--pieceName = 'base',
 				aboveUnit = 100,
 				lightConfig = { posx = 0, posy = 32, posz = 0, radius = 160,
 					dirx = 0, diry = -0.99, dirz = 0.02, theta = 0.4,