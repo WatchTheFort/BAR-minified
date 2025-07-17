beyond-all-reason/Beyond-All-Reason

commit 93834f37db55173d39223649282c877e53e37826

Activate shaders: Move legacy gl.CreateShader to LuaShader (#5454)
Move legacy stuff to LuaShader

lups/particleclasses/shieldsphere.lua
@@ -50,13 +50,14 @@ ShieldSphereParticle.Default = {
 
 local glMultiTexCoord = gl.MultiTexCoord
 local glCallList = gl.CallList
+local LuaShader = gl.LuaShader
 
 local gameFrame = 0
 local timeOffset = 0
 
 function ShieldSphereParticle:BeginDraw()
 	gl.DepthMask(false)
-	gl.UseShader(shieldShader)
+	shieldShader:Activate()
 	gl.Culling(false)
 	gameFrame = Spring.GetGameFrame()
 	timeOffset = Spring.GetFrameTimeOffset()
@@ -64,7 +65,7 @@ end
 
 function ShieldSphereParticle:EndDraw()
 	gl.DepthMask(false)
-	gl.UseShader(0)
+	shieldShader:Deactivate()
 
 	gl.Culling(false)
 
@@ -100,7 +101,7 @@ end
 -----------------------------------------------------------------------------------------------------------------
 
 function ShieldSphereParticle:Initialize()
-	shieldShader = gl.CreateShader({
+	shieldShader = LuaShader({
 		vertex = [[
 			#version 150 compatibility
 
@@ -343,7 +344,7 @@ function ShieldSphereParticle:Initialize()
 			}
 
 		]],
-	})
+	}, "ShieldSphereParticleShader")
 
 	if (shieldShader == nil) then
 		print(PRIO_MAJOR,"LUPS->Shield: critical shader error: "..gl.GetShaderLog())
@@ -354,7 +355,9 @@ function ShieldSphereParticle:Initialize()
 end
 
 function ShieldSphereParticle:Finalize()
-	gl.DeleteShader(shieldShader)
+	if shieldShader then
+		shieldShader:Finalize()
+	end
 	gl.DeleteList(sphereList)
 end
 

luaui/Widgets/gfx_bloom_shader_deferred.lua
@@ -54,8 +54,6 @@ local presets = {
 -- non-editables
 local vsx = 1                        -- current viewport width
 local vsy = 1                        -- current viewport height
-local ivsx = 1.0 / vsx
-local ivsy = 1.0 / vsy
 local qvsx,qvsy
 local iqvsx, iqvsy
 
@@ -85,17 +83,7 @@ local glTexture = gl.Texture
 local glGetShaderLog = gl.GetShaderLog
 local glCreateShader = gl.CreateShader
 local glDeleteShader = gl.DeleteShader
-local glUseShader = gl.UseShader
 
-local glUniformInt = gl.UniformInt
-local glUniform = gl.Uniform
-local glGetUniformLocation = gl.GetUniformLocation
-
-local brightShaderIllumLoc, brightShaderFragLoc
---local brightShaderIvsxLoc, brightShaderIvsyLoc
-local brightShaderTimeLoc
-local blurShaderFragLoc, blurShaderHorizontalLoc
-local combineShaderDebgDrawLoc
 
 local function SetIllumThreshold()
 	local ra, ga, ba = glGetSun("ambient", "unit")
@@ -122,8 +110,8 @@ local function MakeBloomShaders()
 	local viewSizeX, viewSizeY = Spring.GetViewGeometry()
 	local downscale = presets[preset].quality
 	--Spring.Echo("New bloom init preset:", preset)
-	vsx = math.max(4,viewSizeX); ivsx = 1.0 / vsx --we can do /n here!
-	vsy = math.max(4,viewSizeY); ivsy = 1.0 / vsy
+	vsx = math.max(4,viewSizeX)
+	vsy = math.max(4,viewSizeY)
 	qvsx,qvsy = math.ceil(vsx/downscale), math.ceil(vsy/downscale) -- we ceil to ensure perfect upscaling
 	iqvsx, iqvsy = 1.0 / qvsx, 1.0 / qvsy
 
@@ -169,43 +157,44 @@ local function MakeBloomShaders()
 
 
 	if glDeleteShader then
-		if brightShader ~= nil then glDeleteShader(brightShader or 0) end
-		if blurShader ~= nil then glDeleteShader(blurShader or 0) end
-		if combineShader ~= nil then glDeleteShader(combineShader or 0) end
+		if brightShader  then brightShader:Finalize() end
+		if blurShader  	then blurShader:Finalize() end
+		if combineShader  then combineShader:Finalize() end
 	end
 
-	combineShader = glCreateShader({
-		fragment = "#version 150 compatibility\n" .. definesString  ..  [[
-			uniform sampler2D texture0;
-			uniform int debugDraw;
 
-			void main(void) {
-				vec2 subpixel = vec2(IHSX, IHSY) * 0.5; // YES FREAKING HALF-PIXEL BLUR HERE TOO CAUSE WHY NOT?
-				vec4 a = texture2D(texture0, gl_TexCoord[0].st + subpixel);
-				if (debugDraw == 1) {
-					a.a= 1.0;
+	combineShader = LuaShader({
+			fragment = "#version 150 compatibility\n" .. definesString  ..  [[
+				uniform sampler2D texture0;
+				uniform int debugDraw;
+
+				void main(void) {
+					vec2 subpixel = vec2(IHSX, IHSY) * 0.5; // YES FREAKING HALF-PIXEL BLUR HERE TOO CAUSE WHY NOT?
+					vec4 a = texture2D(texture0, gl_TexCoord[0].st + subpixel);
+					if (debugDraw == 1) {
+						a.a= 1.0;
+					}
+					gl_FragColor = a;
+					//gl_FragColor.rg = gl_TexCoord[0].st; // to debug texture coordinates
 				}
-				gl_FragColor = a;
-				//gl_FragColor.rg = gl_TexCoord[0].st; // to debug texture coordinates
-			}
-		]],
-		--while this vertex shader seems to do nothing, it actually does the very important world space to screen space mapping for gl.TexRect!
-		vertex =
-			"#version 150 compatibility\n" .. definesString ..[[
-			void main(void)	{
-				gl_TexCoord[0] = vec4(gl_Vertex.zwzw);
-				#if DOWNSCALE >= 2
-					gl_TexCoord[0].xy = vec2(gl_TexCoord[0].xy * vec2(DOWNSCALE * HSX, DOWNSCALE * HSY ) /  vec2(VSX, VSY));
-				#endif
-				gl_Position    = vec4(gl_Vertex.xy, 0, 1);	}
-		]],
-		uniformInt = {
-			texture0 = 0,
-			debugDraw = 0,
-		}
-	})
+			]],
+			vertex =
+				"#version 150 compatibility\n" .. definesString ..[[
+				void main(void)	{
+					gl_TexCoord[0] = vec4(gl_Vertex.zwzw);
+					#if DOWNSCALE >= 2
+						gl_TexCoord[0].xy = vec2(gl_TexCoord[0].xy * vec2(DOWNSCALE * HSX, DOWNSCALE * HSY ) /  vec2(VSX, VSY));
+					#endif
+					gl_Position    = vec4(gl_Vertex.xy, 0, 1);	}
+			]],
+			uniformInt = {
+				texture0 = 0,
+				debugDraw = 0,
+			},
+		},
+		"Bloom Combine Shader")
 
-	if (combineShader == nil) then
+	if not combineShader:Initialize() then
 		RemoveMe("[BloomShader::Initialize] combineShader compilation failed"); Spring.Echo(glGetShaderLog()); return
 	end
 
@@ -214,7 +203,7 @@ local function MakeBloomShaders()
 	-- this allows us to get away with 5 texture fetches instead of 9 for our 9 sized kernel!
 	 -- TODO:  all this simplification may result in the accumulation of quantizing errors due to the small numbers that get pushed into the BrightTexture
 
-	blurShader = glCreateShader({
+	blurShader = LuaShader({
 		vertex = [[
 			#version 150 compatibility
 			void main(void)	{
@@ -371,15 +360,16 @@ local function MakeBloomShaders()
 		},
 		uniformFloat = {
 			horizontal = 0,
+			fragBlurAmplifier = 0,
 		}
-	})
+	}, "Bloom Blur Shader")
 
-	if (blurShader == nil) then
+	if not blurShader:Initialize() then
 		RemoveMe("[BloomShader::Initialize] blurShader compilation failed"); Spring.Echo(glGetShaderLog()); return
 	end
 
 
-	brightShader = glCreateShader({
+	brightShader = LuaShader({
 		vertex = [[
 			#version 150 compatibility
 			void main(void)	{
@@ -397,8 +387,6 @@ local function MakeBloomShaders()
 
 			uniform float illuminationThreshold;
 			uniform float fragGlowAmplifier;
-			//uniform float ivsx;
-			//uniform float ivsy;
 			uniform float time;
 
 			void main(void) {
@@ -486,27 +474,16 @@ local function MakeBloomShaders()
 			mapDepthTex = 3,
 		},
 		uniformFloat = {
-			--ivsx = 0,
-			--ivsy = 0,
 			time = 0,
+			illuminationThreshold = 0, 
+			fragGlowAmplifier = 0,
 		}
-	})
+	}, "Bloom Bright Shader")
 
-	if (brightShader == nil) then
+	if not brightShader:Initialize() then
 		Spring.Echo(glGetShaderLog());
 		RemoveMe("[BloomShader::Initialize] brightShader compilation failed"); return
 	end
-
-	--brightShaderIvsxLoc = glGetUniformLocation(brightShader, "ivsx")
-	--brightShaderIvsyLoc = glGetUniformLocation(brightShader, "ivsy")
-	brightShaderTimeLoc = glGetUniformLocation(brightShader, "time")
-	brightShaderIllumLoc = glGetUniformLocation(brightShader, "illuminationThreshold")
-	brightShaderFragLoc = glGetUniformLocation(brightShader, "fragGlowAmplifier")
-
-	blurShaderFragLoc = glGetUniformLocation(blurShader, "fragBlurAmplifier")
-	blurShaderHorizontalLoc = glGetUniformLocation(blurShader, "horizontal")
-
-	combineShaderDebgDrawLoc = glGetUniformLocation(combineShader, "debugDraw")
 
 end
 
@@ -555,9 +532,9 @@ function widget:Shutdown()
 	glDeleteTexture(brightTexture2)
 	brightTexture1, brightTexture2 = nil, nil
 	if glDeleteShader then
-		if brightShader ~= nil then glDeleteShader(brightShader or 0) end
-		if blurShader ~= nil then glDeleteShader(blurShader or 0) end
-		if combineShader ~= nil then glDeleteShader(combineShader or 0) end
+		if brightShader  then brightShader:Finalize() end
+		if blurShader ~= nil then blurShader:Finalize() end
+		if combineShader ~= nil then combineShader:Finalize() end
 	end
 	WG['bloomdeferred'] = nil
 end
@@ -576,12 +553,11 @@ local function Bloom()
 	gl.Color(1, 1, 1, 1)
 	gl.Culling(true)
 
-	glUseShader(brightShader)
-		glUniform(   brightShaderIllumLoc, illumThreshold)
-		glUniform(   brightShaderFragLoc, glowAmplifier)
-		--glUniform(   brightShaderIvsxLoc, 0.5/qvsx)
-		--glUniform(   brightShaderIvsyLoc, 0.5/qvsy)
-		glUniform(   brightShaderTimeLoc, df)
+	brightShader:Activate()
+		brightShader:SetUniform("illuminationThreshold", illumThreshold)
+		brightShader:SetUniform("fragGlowAmplifier", glowAmplifier)
+		--brightShader:SetUniform("time", df)
+
 		glTexture(0, "$model_gbuffer_difftex")
 		glTexture(1, "$model_gbuffer_emittex")
 		glTexture(2, "$model_gbuffer_zvaltex")
@@ -594,28 +570,26 @@ local function Bloom()
 		glTexture(1, false)
 		glTexture(2, false)
 		glTexture(3, false)
-	glUseShader(0)
+	brightShader:Deactivate()
 
 	if not debugBrightShader then
 		if presets[preset].blurPasses > 0 then
-			glUseShader(blurShader)
+			blurShader:Activate()
 			for i = 1, presets[preset].blurPasses do
-
-					glUniform(blurShaderFragLoc, blurAmplifier)
-					glUniform(blurShaderHorizontalLoc, 0)
+					blurShader:SetUniform("fragBlurAmplifier", blurAmplifier)
+					blurShader:SetUniform("horizontal", 0)
 					glTexture(brightTexture1)
 					--glRenderToTexture(brightTexture2, gl.TexRect, -1, 1, 1, -1)
 					glRenderToTexture(brightTexture2, FullScreenQuad)
 					glTexture(false)
 
-					glUniform(blurShaderFragLoc, blurAmplifier)
-					glUniform(blurShaderHorizontalLoc, 1)
+					blurShader:SetUniform("horizontal", 1)
 					glTexture(brightTexture2)
 					--glRenderToTexture(brightTexture1, gl.TexRect, -1, 1, 1, -1)
 					glRenderToTexture(brightTexture1, FullScreenQuad)
 					glTexture(false)
 			end
-			glUseShader(0)
+			blurShader:Deactivate()
 		end
 	end
 
@@ -624,14 +598,13 @@ local function Bloom()
 	else
 		gl.Blending(GL.ONE, GL.ZERO)
 	end
-
-	glUseShader(combineShader)
-		glUniformInt(combineShaderDebgDrawLoc, dbgDraw)
+	combineShader:Activate()
+		combineShader:SetUniformInt("debugDraw",dbgDraw)
 		glTexture(0, brightTexture1)
 		--gl.TexRect(-1, -1, 1, 1, 0, 0, 1, 1)
 		rectVAO:DrawArrays(GL.TRIANGLES)
 		glTexture(0, false)
-	glUseShader(0)
+	combineShader:Deactivate()
 
 	gl.Blending("reset")
 	gl.DepthMask(false) --"BK OpenGL state resets", was true

luaui/Widgets/gfx_cas.lua
@@ -202,7 +202,7 @@ function widget:Initialize()
 		uniformInt = {
 			screenCopyTex = 0,
 		},
-	}, ": Contrast Adaptive Sharpen")
+	}, "Contrast Adaptive Sharpen")
 
 	local shaderCompiled = casShader:Initialize()
 	if not shaderCompiled then

luaui/Widgets/gfx_guishader.lua
@@ -23,6 +23,8 @@ local defaultBlurIntensity = 1
 
 -- hardware capability
 local canShader = gl.CreateShader ~= nil
+
+local LuaShader = gl.LuaShader
 local NON_POWER_OF_TWO = gl.HasExtension("GL_ARB_texture_non_power_of_two")
 
 local renderDlists = {}
@@ -33,9 +35,6 @@ local screencopyUI -- this is for the special case of UI blur
 
 local stenciltex
 local stenciltexScreen
-local intensityLoc
-local ivsxLoc
-local ivsyLoc
 
 local screenBlur = false
 
@@ -154,11 +153,11 @@ end
 
 local function CreateShaders()
 	if blurShader then
-		gl.DeleteShader(blurShader or 0)
+		blurShader:Finalize()
 	end
 
 	-- create blur shaders
-	blurShader = gl.CreateShader({
+	blurShader = LuaShader({
 		fragment = [[
 		#version 150 compatibility
 		uniform sampler2D tex2;
@@ -230,18 +229,16 @@ local function CreateShaders()
 			ivsx = 0,
 			ivsy = 0,
 		}
-	})
+	}, "guishader blurShader")
 
 
-	if blurShader == nil then
+	if not blurShader:Initialize() then
 		Spring.Log(widget:GetInfo().name, LOG.ERROR, "guishader blurShader: shader error: " .. gl.GetShaderLog())
 		widgetHandler:RemoveWidget()
 		return false
 	end
 
-	intensityLoc = gl.GetUniformLocation(blurShader, "intensity")
-	ivsxLoc = gl.GetUniformLocation(blurShader, "ivsx")
-	ivsyLoc = gl.GetUniformLocation(blurShader, "ivsy")
+
 
 	screencopyUI = gl.CreateTexture(vsx, vsy, {
 		border = false,
@@ -264,9 +261,7 @@ local function DeleteShaders()
 	gl.DeleteTexture(usedStencilTex)
 	gl.DeleteTexture(screencopyUI)
 	stenciltex, stenciltexScreen, screencopyUI, usedStencilTex = nil, nil, nil, nil
-	if gl.DeleteShader then
-		gl.DeleteShader(blurShader or 0)
-	end
+	if blurShader then blurShader:Finalize() end
 	blurShader = nil
 end
 
@@ -309,14 +304,14 @@ function widget:DrawScreenEffects() -- This blurs the world underneath UI elemen
 		gl.Blending(true)
 		gl.Texture(screencopy)
 		gl.Texture(2, stenciltex)
-		gl.UseShader(blurShader)
+		blurShader:Activate()
+			--blurShader:SetUniform("intensity", math.max(blurIntensity, 0.0015))
+			blurShader:SetUniform("ivsx", 0.5/vsx)
+			blurShader:SetUniform("ivsy", 0.5/vsy)
 
-		gl.Uniform(intensityLoc, math.max(blurIntensity, 0.0015))
-		gl.Uniform(ivsxLoc, 0.5/vsx)
-		gl.Uniform(ivsyLoc, 0.5/vsy)
+			gl.TexRect(0, vsy, vsx, 0) -- draw the blurred version
+		blurShader:Deactivate()
 
-		gl.TexRect(0, vsy, vsx, 0) -- draw the blurred version
-		gl.UseShader(0)
 		gl.Texture(2, false)
 		gl.Texture(false)
 		gl.Blending(false)
@@ -342,14 +337,14 @@ local function DrawScreen() -- This blurs the UI elements obscured by other UI e
 		gl.Texture(screencopyUI)
 
 		gl.Texture(2, stenciltexScreen)
-		gl.UseShader(blurShader)
 
-		gl.Uniform(intensityLoc, math.max(blurIntensity, 0.0015))
-		gl.Uniform(ivsxLoc, 0.5/vsx)
-		gl.Uniform(ivsyLoc, 0.5/vsy)
+		blurShader:Activate()
+			--blurShader:SetUniform("intensity", math.max(blurIntensity, 0.0015))
+			blurShader:SetUniform("ivsx", 0.5/vsx)
+			blurShader:SetUniform("ivsy", 0.5/vsy)
 
-		gl.TexRect(0, vsy, vsx, 0) -- draw the blurred version
-		gl.UseShader(0)
+			gl.TexRect(0, vsy, vsx, 0) -- draw the blurred version
+		blurShader:Deactivate()
 		gl.Texture(2, false)
 		gl.Texture(false)
 	end

luaui/Widgets/gfx_snow.lua
@@ -77,10 +77,6 @@ table.insert(particleTypes, {
 local widgetDisabledSnow = false
 
 local shader
-local shaderTimeLoc
-local shaderCamPosLoc
-local shaderScaleLoc
-local shaderSpeedLoc
 
 local startTimer = Spring.GetTimer()
 local diffTime = 0
@@ -118,6 +114,7 @@ local glDeleteList         = gl.DeleteList
 local glTexture            = gl.Texture
 local glGetShaderLog       = gl.GetShaderLog
 local glCreateShader       = gl.CreateShader
+local LuaShader            = gl.LuaShader
 local glDeleteShader       = gl.DeleteShader
 local glUseShader          = gl.UseShader
 local glUniform            = gl.Uniform
@@ -195,7 +192,7 @@ local function init()
 		return
 	end
 
-	shader = glCreateShader({
+	shader = LuaShader({
 		vertex = [[
 	  		#version 150 compatibility
 			uniform float time;
@@ -235,26 +232,20 @@ local function init()
 				gl_Position = gl_ProjectionMatrix * eyePos;
 			}
 		]],
-		uniform = {
+		uniformFloat = {
 			time   = diffTime,
 			scale  = 0,
 			speed  = {0,0,0},
 			camPos = {0,0,0},
 		},
-	})
+	}, "Snow Shader")
 
-	if (shader == nil) then
+	if not shader:Initialize() then
 		Spring.Echo("[Snow widget:Initialize] particle shader compilation failed")
 		Spring.Echo(glGetShaderLog())
 		widgetHandler:RemoveWidget()
 		return
 	end
-
-	shaderTimeLoc			= glGetUniformLocation(shader, 'time')
-	shaderCamPosLoc			= glGetUniformLocation(shader, 'camPos')
-
-	shaderScaleLoc			= glGetUniformLocation(shader, 'scale')
-	shaderSpeedLoc			= glGetUniformLocation(shader, 'speed')
 
 	if particleLists[1] == nil then
 		CreateParticleLists()
@@ -400,6 +391,7 @@ end
 function widget:Shutdown()
 	enabled = false
 	widgetHandler:RemoveAction("snow")
+	if shader then shader:Finalize() end 
 end
 
 local pausedTime = 0
@@ -415,13 +407,12 @@ function widget:DrawWorld()
 	lastFrametime = Spring.GetTimer()
 	if os.clock() - startOsClock > 0.5 then		-- delay to prevent no textures being shown
 		if shader ~= nil and particleLists[#particleTypes] ~= nil and particleLists[#particleTypes][particleStep] ~= nil then
-			glUseShader(shader)
+			shader:Activate()
 			camX,camY,camZ = Spring.GetCameraPosition()
 			diffTime = Spring.DiffTimers(lastFrametime, startTimer) - pausedTime
-
-			glUniform(shaderTimeLoc,diffTime * 1)
-			glUniform(shaderCamPosLoc, camX, camY, camZ)
-
+			shader:SetUniform("time", diffTime)
+			shader:SetUniform("camPos", camX, camY, camZ)
+			
 			glDepthTest(true)
 			glBlending(GL.SRC_ALPHA, GL.ONE)
 
@@ -440,8 +431,8 @@ function widget:DrawWorld()
 
 			glTexture(snowTexture)
 			for particleType, pt in pairs(particleTypes) do
-				glUniform(shaderScaleLoc, pt.scale*particleScale)
-				glUniform(shaderSpeedLoc, pt.gravity, offsetX, offsetZ)
+				shader:SetUniform("scale", pt.scale * particleScale)
+				shader:SetUniform("speed", pt.gravity, offsetX, offsetZ)
 				glCallList(particleLists[particleType][particleStep])
 			end
 			glTexture(false)
@@ -450,7 +441,7 @@ function widget:DrawWorld()
 			gl.PointSize(1.0)
 			gl.PointSprite(false, false)
 			glResetState()
-			glUseShader(0)
+			shader:Deactivate()
 		end
 	end
 end

luaui/Widgets/gfx_volumetric_clouds.lua
@@ -104,14 +104,9 @@ local glBlending             = gl.Blending
 local glCopyToTexture        = gl.CopyToTexture
 local glCreateShader         = gl.CreateShader
 local glCreateTexture        = gl.CreateTexture
-local glDeleteShader         = gl.DeleteShader
 local glDeleteTexture        = gl.DeleteTexture
-local glGetShaderLog         = gl.GetShaderLog
-local glGetUniformLocation   = gl.GetUniformLocation
 local glTexture              = gl.Texture
-local glUniform              = gl.Uniform
-local glUniformMatrix        = gl.UniformMatrix
-local glUseShader            = gl.UseShader
+local LuaShader 			 = gl.LuaShader
 local spGetCameraPosition    = Spring.GetCameraPosition
 local spGetWind              = Spring.GetWind
 
@@ -536,7 +531,7 @@ end
 local function init()
 
 	if depthShader then
-		glDeleteShader(depthShader)
+		depthShader:Finalize()
 	end
 
 	fragSrc = fragSrc:format(
@@ -550,7 +545,7 @@ local function init()
 	fragSrc = fragSrc:gsub("###CLAMP_TO_MAP###", tostring((cloudsClamp and 1) or 0))
 
 	if enabled then
-		depthShader = glCreateShader({
+		depthShader = LuaShader({
 			vertex = vertSrc,
 			fragment = fragSrc,
 			uniformInt = {
@@ -558,19 +553,19 @@ local function init()
 				tex1 = 1,
 				tex2 = 2,
 			},
-		})
+			uniformFloat = {
+				eyePos = {0,0,0},
+				viewProjectionInv = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,1}, -- will be set later;
+				offset = {0,0,0}, -- will be set later;
+				sundir = {0,0,0}, -- will be set later;
+				suncolor = {0,0,0}, -- will be set later;
+				time = 0
+			},
+		}, "Volumetric Clouds Depth Shader")
 
-		spEcho(glGetShaderLog())
-		if not depthShader then
+		if not depthShader:Initialize() then
 			spEcho("Bad shader, reverting to non-GLSL widget.")
 			enabled = false
-		else
-			uniformEyePos       = glGetUniformLocation(depthShader, 'eyePos')
-			uniformViewPrjInv   = glGetUniformLocation(depthShader, 'viewProjectionInv')
-			uniformOffset       = glGetUniformLocation(depthShader, 'offset')
-			uniformSundir       = glGetUniformLocation(depthShader, 'sundir')
-			uniformSunColor     = glGetUniformLocation(depthShader, 'suncolor')
-			uniformTime         = glGetUniformLocation(depthShader, 'time')
 		end
 	end
 end
@@ -609,8 +604,8 @@ function widget:Shutdown()
 	glDeleteTexture(depthTexture)
 	glDeleteTexture(fogTexture)
 	depthTexture, fogTexture = nil, nil
-	if glDeleteShader then
-		glDeleteShader(depthShader)
+	if depthShader then
+		depthShader:Finalize()
 	end
 	glDeleteTexture(noiseTex)
 	glDeleteTexture(noiseTex3D)
@@ -634,25 +629,27 @@ local function DrawFogNew()
 	glCopyToTexture(depthTexture, 0, 0, vpx, vpy, vsx, vsy) --FIXME scale down?
 
 	-- setup the shader and its uniform values
-	glUseShader(depthShader)
+	depthShader:Activate()
 
-	-- set uniforms
-	glUniform(uniformEyePos, spGetCameraPosition())
-	glUniform(uniformOffset, offsetX, offsetY, offsetZ)
+		-- set uniforms
+		depthShader:SetUniform("eyePos", spGetCameraPosition())
+		depthShader:SetUniform("offset", offsetX, offsetY, offsetZ)
 
-	glUniform(uniformSundir, sunDir[1], sunDir[2], sunDir[3])
-	glUniform(uniformSunColor, sunCol[1], sunCol[2], sunCol[3])
+		depthShader:SetUniform("sundir", sunDir[1], sunDir[2], sunDir[3])
+		depthShader:SetUniform("suncolor", sunCol[1], sunCol[2], sunCol[3])
 
-	glUniform(uniformTime, Spring.GetGameSeconds() * speed)
+		depthShader:SetUniform("time", Spring.GetGameSeconds() * speed)
 
-	glUniformMatrix(uniformViewPrjInv,  "viewprojectioninverse")
+		depthShader:SetUniformMatrix("viewProjectionInv", "viewprojectioninverse")
 
-	-- TODO: completely reset the texture before applying shader
-	-- TODO: figure out why it disappears in some places
-	-- maybe add a switch to make it high-res direct-render
-	gl.RenderToTexture(fogTexture, renderToTextureFunc)
+		--glUniformMatrix(uniformViewPrjInv,  "viewprojectioninverse")
 
-	glUseShader(0)
+		-- TODO: completely reset the texture before applying shader
+		-- TODO: figure out why it disappears in some places
+		-- maybe add a switch to make it high-res direct-render
+		gl.RenderToTexture(fogTexture, renderToTextureFunc)
+
+	depthShader:Deactivate()
 end
 
 

WatchTheFort/BAR-minified

commit 8ad6ad6ba2930a0be1e1b4b54f419e11163e3c0e

Add missing shieldShader:Initialize to shieldsphere.lua. (#5536)
### Work done

- Add missing shieldShader:Initialize to shieldsphere.lua.


#### Addresses Issue(s)

```
[t=00:01:02.306990][f=-000001] LuaShader: [ShieldSphereParticleShader] shader error(s):
Attempt to use invalid shader object in [Finalize](). Did you call :Compile() or :Initialize()
[t=00:01:02.307375][f=-000001] Attempt to use invalid shader object in [Finalize](). Did you call :Compile() or :Initialize()
```

### Remarks

- Probably broken since
https://github.com/beyond-all-reason/Beyond-All-Reason/pull/5454 was
merged at e75bd0b64a079ca6fb43772b47a5eb0374439cfa
- Unsure whether this shader is actually used, maybe should be removed
instead. Maybe @Ruwetuin or @Beherith know.
- Otherwise safe to simply fix for now as I'm doing here.

lups/particleclasses/shieldsphere.lua
@@ -350,6 +350,7 @@ function ShieldSphereParticle:Initialize()
 		print(PRIO_MAJOR,"LUPS->Shield: critical shader error: "..gl.GetShaderLog())
 		return false
 	end
+	shieldShader:Initialize()
 
 	sphereList = gl.CreateList(DrawSphere, 0, 0, 0, 1, 30, false)
 end